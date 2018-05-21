e8dcf2076e14c52f828b7bf7ed2d368745e5786f
Flavio <f.giordani003@studenti.unibs.it>
2018-05-21 16:37:05 +0200

    Creati metodi base per Edge, Node, Graph

--- src/program/Graph.java
+++ src/program/Graph.java
@@ -1,5 +1,30 @@
 package program;
 
+import java.util.LinkedList;
+
 public class Graph {
+    private LinkedList<Node> nodes = new LinkedList<>();
+    private LinkedList<Edge> edges = new LinkedList<>();
+    private LinkedList<Integer> idList = new LinkedList<>();
+
+    public Graph() {
+
+    }
+
+    public void addNode(Node newNode) {
+        nodes.add(newNode);
+    }
+
+    public void addEdge(Node from, Node to) {
+        edges.add(new Edge(from, to));
+    }
+
+    public Node searchNode(int id) {
+        for (Node element : nodes){
+            if(element.getId() == id)
+                return element;
+        }
+        return null;
+    }
 
 }
--- src/program/Node.java
+++ src/program/Node.java
@@ -1,5 +1,57 @@
 package program;
 
-public class Node {
+import java.util.LinkedList;
+import java.util.List;
 
+public class Node {
+    private String name;
+    private double x;
+    private double y;
+    private double altitude;
+    private int id;
+    private List<Node> adjacentNodes = new LinkedList<>();
+
+
+    public Node(String name, double x, double y, double altitude, int id) {
+        this.name = name;
+        this.x = x;
+        this.y = y;
+        this.altitude = altitude;
+        this.id = id;
+    }
+
+    public Node(int id) {
+        this.id = id;
+    }
+
+    public double getAltitude() {
+        return altitude;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void addAdjacentNodes(Node adj) {
+        adjacentNodes.add(adj);
+    }
+
+    public void init(String name, double x, double y, double altitude) {
+        this.name = name;
+        this.x = x;
+        this.y = y;
+        this.altitude = altitude;
+    }
 }
